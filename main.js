9db8cab09466315bc1a5e9b0c9d3b903beb450d9
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-09-12 16:07:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -122,3 +122,56 @@ function getValues(e){
 	/*let myobjDestringify = JSON.parse(localStorage.getItem("myObj"));
 	console.log(myobjDestringify);*/
 }
+
+
+//remove item using delete button and local storage;
+
+
+var form = document.getElementById('my-form');
+
+form.addEventListener('submit',getValues);
+
+function getValues(e){
+	e.preventDefault();
+
+	var name1 = document.getElementById('name').value;
+	var email1 = document.getElementById('email').value;
+	var phone  = document.getElementById('phone').value;
+	//localStorage.setItem('name',name);
+	// localStorage.setItem('email',email);
+	var itemList = document.getElementById('list');
+
+	var li = document.createElement('li');
+	li.className='item';
+	var delbutton  = document.createElement('button');
+	delbutton.className= 'float-right delete';
+	delbutton.appendChild(document.createTextNode('delete'));
+	li.appendChild(document.createTextNode(`${name1}:${email1}:${phone}`));
+	//console.log(li.textContent);
+	li.appendChild(delbutton);
+	itemList.appendChild(li);
+
+	let myObj = {
+		name : name1,
+		email : email1,
+		phone : phone
+	};
+	//console.log(myObj);
+	let myObjStringify = JSON.stringify(myObj)
+	localStorage.setItem(email1,myObjStringify);
+}
+var itemList = document.getElementById('list');
+itemList.addEventListener('click',removeItem1);
+
+function removeItem1(e){
+	e.preventDefault();
+
+	if(e.target.classList.contains('delete')){
+		var li = e.target.parentElement;
+		var text = li.textContent;
+		var individualText = text.split(':');
+		var email=individualText[1]
+		localStorage.removeItem(email);
+		itemList.removeChild(li);
+	}
+}
